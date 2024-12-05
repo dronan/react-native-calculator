07c47263a275d875c96db2496bdd86697e5fb284
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-05 14:06:46 +0100

    criando componente de display

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 import Button from './src/components/Button'
+import Display from './src/components/Display';
 
 export default class App extends Component  {
+
+  state = {
+    displayValue: '0'
+  }
+
   render() {
     return (
           <View
             style={styles.sectionContainer}
             >
+              <Display value={this.state.displayValue} />
               <View style={styles.buttons}>
                 <Button label='AC'/>
                 <Button label='/'/>
